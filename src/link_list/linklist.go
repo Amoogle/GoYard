58e436eda4b461e980d895df52b11e5f96376dde
Amgoole <xwen.xi@icloud.com>
2020-09-13 13:09:54 +0800

    单链表插入和逆序方法实现

--- src/link_list/linklist.go
+++ src/link_list/linklist.go
@@ -22,7 +22,8 @@ func CreateHead(head *LinkNode, array []int) *LinkNode {
 
 func CreateTail(head *LinkNode, array []int) *LinkNode {
 	temp := head
-	for _, data := range array {
+	head.data = array[0]
+	for _, data := range array[1:] {
 		node := LinkNode{data: data}
 		if head != nil {
 			head.next = &node
@@ -39,27 +40,68 @@ func Travel(head *LinkNode) {
 	}
 }
 
-func InertNode(head *LinkNode, index int, data interface{}) bool {
+// 插入指定节点
+func InsertNode(head *LinkNode, index int, data interface{}) bool {
+	// 函数体内head指针为实参的一个拷贝
 	if index < 0 {
 		return false
-	} else {
-		node := LinkNode{data: data}
-		i := 0
-		for ; head.next != nil && i < index; head = head.next {
-			i++
-		}
-		if i >= index && head != nil {
-			if i == 0 {
-				node.next = head
-				head.data, node.data = node.data, head.data
-			}
-			if i > 0 {
-				node.next = head.next
-				head.next = &node
-			}
-		} else {
-			return false
-		}
+	}
+	node := LinkNode{data: data}
+
+	// 插入节点到链表首位
+	if index == 0 {
+		node.next = head.next
+		head.next = &node
+		head.data, node.data = node.data, head.data
 		return true
 	}
+
+	i := 0
+	for ; head.next != nil && i < index-1; head = head.next {
+		i++
+	}
+
+	// 插入点超出链表最大可插入点
+	if head.next == nil && i < index-1 {
+		return false
+	}
+
+	// 插入到中间节点
+	if head.next != nil {
+		node.next = head.next
+		head.next = &node
+		return true
+	}
+
+	// 插入到尾节点
+	if head.next == nil {
+		head.next = &node
+		return true
+	}
+	return false
+}
+
+func ReverseLink(head *LinkNode) *LinkNode {
+	cur := head.next
+	next := head.next.next
+	head.next = nil
+	for ; next != nil; next = next.next {
+		cur.next = head
+		head = cur
+		cur = next
+	}
+	cur.next = head
+	return cur
+}
+
+// 递归逆转 传入o 作为逆转后的链头
+func RReverse(o *LinkNode, head *LinkNode) *LinkNode {
+	if head.next == nil {
+		o.next = head
+		return head
+	}
+	node := RReverse(o, head.next)
+	head.next = nil
+	node.next = head
+	return head
 }
--- src/mian.go
+++ src/mian.go
@@ -6,21 +6,28 @@ import (
 )
 
 func main() {
-	node := linklist.Node{}
+	//node := linklist.Node{}
 	cycleLink := linklist.OneCycleList{}
 	cycleLink.Append(2)
 	array := []int{1, 2, 3, 4, 5}
-	linklist.Reverse(&node, &cycleLink)
+	//linklist.Reverse(&node, &cycleLink)
 
 	link1 := linklist.LinkNode{}
 	head1 := linklist.CreateHead(&link1, array)
-	linklist.InertNode(head1, 8, 99)
+	linklist.InsertNode(head1, 8, 99)
 	linklist.Travel(&link1)
 
 	link2 := linklist.LinkNode{}
 	head2 := linklist.CreateTail(&link2, array)
-	linklist.InertNode(head2, 0, 99)
+	linklist.Travel(head2)
+	linklist.InsertNode(head2, 5, 99)
 	linklist.Travel(&link2)
 
+	rlink := linklist.ReverseLink(&link2)
+	linklist.Travel(rlink)
+
+	o := new(linklist.LinkNode)
+	linklist.RReverse(o, rlink)
+	linklist.Travel(o)
 	fmt.Println("hello go word")
 }
